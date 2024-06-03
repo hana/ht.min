ba67df172ef2fd7aa8ca7285fb5c245b245ecec3
Hananosuke Takimoto <hana.1995@mac.com>
2024-06-04 02:11:32 +0900

    Update path

--- externals/reset-codesign.sh
+++ externals/reset-codesign.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-
+PATH_DIR_SCRIPT="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
+pushd $PATH_DIR_SCRIPT
+xattr -cr .
 for mxo in externals/*.mxo
 do
     xattr -cr "${mxo}"
     codesign --force --deep --sign - "${mxo}"
 done
+popd
 
