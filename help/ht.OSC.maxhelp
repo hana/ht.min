f150105b505204bf0cee0a6328affd5019e2f143
Hananosuke Takimoto <hana.1995@mac.com>
2024-07-25 00:39:20 +0400

    ht.OSC v1.0.0 implemented

---
+++ help/ht.OSC.maxhelp
@@ -0,0 +1,234 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 8,
+			"minor" : 6,
+			"revision" : 4,
+			"architecture" : "x64",
+			"modernui" : 1
+		}
+,
+		"classnamespace" : "box",
+		"rect" : [ 100.0, 100.0, 519.0, 386.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Lato Light",
+		"gridonopen" : 1,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 2,
+		"objectsnaponopen" : 0,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"lefttoolbarpinned" : 2,
+		"toptoolbarpinned" : 2,
+		"righttoolbarpinned" : 2,
+		"bottomtoolbarpinned" : 2,
+		"toolbars_unpinned_last_save" : 15,
+		"tallnewobj" : 0,
+		"boxanimatetime" : 200,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"style" : "tap",
+		"subpatcher_template" : "tap.template",
+		"assistshowspatchername" : 0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"border" : 0,
+					"filename" : "helpdetails.js",
+					"id" : "obj-6",
+					"ignoreclick" : 1,
+					"jsarguments" : [ "ht.OSC" ],
+					"maxclass" : "jsui",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 0.0, 0.0, 465.0, 105.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-18",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 60.0, 330.0, 405.0, 23.0 ],
+					"text" : "/foo 1.23 46"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-16",
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 107.0, 289.0, 194.0, 21.0 ],
+					"text" : "and \"encode\" converts binary to list."
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-15",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 15.0, 288.0, 87.0, 23.0 ],
+					"text" : "ht.OSC encode"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-14",
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 107.5, 205.0, 250.0, 21.0 ],
+					"text" : "\"decode\" mode converts OSC-like list to binary"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-11",
+					"maxclass" : "newobj",
+					"numinlets" : 3,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 15.0, 162.0, 155.0, 23.0 ],
+					"text" : "pak /foo 1. 1"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-10",
+					"maxclass" : "number",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 151.0, 120.0, 50.0, 23.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"format" : 6,
+					"id" : "obj-8",
+					"maxclass" : "flonum",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 83.0, 120.0, 50.0, 23.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-3",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 60.0, 246.0, 405.0, 23.0 ],
+					"text" : "47 102 111 111 0 0 0 0 44 102 105 0 63 157 112 164 0 0 0 46"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 15.0, 204.0, 87.0, 23.0 ],
+					"text" : "ht.OSC decode"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-15", 0 ],
+					"order" : 1,
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-3", 1 ],
+					"order" : 0,
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-11", 2 ],
+					"source" : [ "obj-10", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-11", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-18", 1 ],
+					"source" : [ "obj-15", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-11", 1 ],
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [ 			{
+				"name" : "helpdetails.js",
+				"bootpath" : "C74:/help/resources",
+				"type" : "TEXT",
+				"implicit" : 1
+			}
+, 			{
+				"name" : "ht.OSC.mxo",
+				"type" : "iLaX"
+			}
+ ],
+		"autosave" : 0,
+		"styles" : [ 			{
+				"name" : "tap",
+				"default" : 				{
+					"fontname" : [ "Lato Light" ]
+				}
+,
+				"parentstyle" : "",
+				"multi" : 0
+			}
+ ]
+	}
+
+}
---
+++ source/projects/ht.OSC/ht.osc.cpp
@@ -0,0 +1,168 @@
+/// @file
+///	@ingroup 	minexamples
+///	@copyright	Copyright 2018 The Min-DevKit Authors. All rights reserved.
+///	@license	Use of this source code is governed by the MIT License found in the License.md file.
+
+#include "c74_min.h"
+
+using namespace c74::min;
+
+#include <vector>
+#include <string>
+
+#include "oscpp/server.hpp"
+#include "oscpp/client.hpp"
+
+class ht_OSC : public object<ht_OSC> {
+private:
+    c74::min::atoms encode(const c74::min::atoms& packet) {    // binary to list
+        std::vector<uint8_t> p;
+        p.reserve(packet.size());
+        
+        constexpr auto min = std::numeric_limits<uint8_t>::min();
+        constexpr auto max = std::numeric_limits<uint8_t>::max();
+        
+        for(const auto& byte : packet) {
+            const int val = static_cast<int>(byte);
+            if(max < val) {
+                p.emplace_back(max);
+            } else if (val < min) {
+                p.emplace_back(min);
+            } else {
+                p.emplace_back(val);
+            }
+        }
+        
+        OSCPP::Server::Message msg (OSCPP::Server::Packet(p.data(), p.size()));
+        OSCPP::Server::ArgStream args(msg.args());
+                        
+        atoms atm;
+        atm.emplace_back(msg.address());
+                                
+        while(!args.atEnd()) {
+            const auto tag = args.tag();
+            switch(tag) {
+                case 'i':
+                    atm.emplace_back(args.int32());
+                    break;
+                case 'f':
+                    atm.emplace_back(args.float32());
+                    break;
+                case 's':
+                    atm.emplace_back(args.string());
+                    break;
+                case 'b':
+                {
+                    const auto blob = args.blob();
+                    const auto size = blob.size();
+                    std::vector<uint8_t> data(size, 0);
+                    std::memcpy(data.data(), blob.data(), size);
+                    atm.emplace_back("OSCBlob");
+                    atm.emplace_back(size);
+                    const auto a = to_atoms(data);
+                    atm.reserve(atm.size() + a.size());
+                    std::copy(a.begin(),a.end(),std::back_inserter(atm));
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+        return atm;
+    }
+    
+    c74::min::atoms decode(const c74::min::atoms& atms) {   // list to binary
+        static std::vector<uint8_t> buffer;
+        buffer.resize(2048);
+        OSCPP::Client::Packet packet(buffer.data(), buffer.size());
+        
+        const auto adr = static_cast<std::string>(atms[0]);
+        const auto& num_args = atms.size() - 1;
+        
+        packet.openMessage(adr.c_str(), num_args);
+                        
+        for(auto i = 1; i < atms.size(); i++) {
+            const auto& arg = atms[i];
+            switch(arg.type()) {
+                case c74::min::message_type::int_argument:
+                    packet.int32(arg);
+                    break;
+                case c74::min::message_type::float_argument:
+                    packet.float32(arg);
+                    break;
+                case c74::min::message_type::symbol_argument:
+                    packet.string(static_cast<std::string>(arg).c_str());
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        packet.closeMessage();
+        buffer.resize(packet.size());
+                    
+        return to_atoms(buffer);
+    }
+    
+    function process = MIN_FUNCTION {
+        switch(operation) {
+            case operations::encode:
+                if(args[0].type() == c74::min::message_type::symbol_argument) {
+                    cerr << "Decode only supports binaries" << endl;
+                    return{};
+                }
+                output.send(encode(args));
+                break;
+            case operations::decode:
+                output.send(decode(args));
+                break;
+            default:
+                break;
+        }
+
+        return {};
+    };
+    
+public:
+    MIN_DESCRIPTION	{"Encode list to binary, decode binary to list."};
+    MIN_TAGS		{"utilities"};
+    MIN_AUTHOR		{"Hananosuke Takimoto"};
+    MIN_RELATED		{"udpreceive, udpsend, ht.udpreceive"};
+    
+    inlet<>  input	{ this, "(bang) output the unix time" };
+    outlet<> output	{ this, "(int) output the unix time" };
+    
+    enum class operations : int {encode, decode, enum_count};
+    enum_map operation_range = {"encode", "decode"};
+    
+    argument<symbol> operation_arg = {this, "operation", "Operation mode - encode from binary to list or decode list to binary.",
+        MIN_ARGUMENT_FUNCTION {
+            if (arg == "decode") {
+                operation = operations::decode;
+            } else {
+                operation = operations::encode;
+            }
+        }
+    };
+    
+    attribute<operations> operation { this, "operation", operations::encode, operation_range,
+        description {
+            "Choose the operation to perfome with the input. encode or decode."
+        }
+    };
+    
+    message<threadsafe::yes> list {this, "list", "binary or list", process};
+    message<threadsafe::yes> anything {this, "anything", "binary or list", process};
+
+    // post to max window == but only when the class is loaded the first time
+    message<> maxclass_setup { this, "maxclass_setup",
+        MIN_FUNCTION {
+            cout << "ht.OSC v1.0.0" << endl;
+            return {};
+        }
+    };
+
+};
+
+
+MIN_EXTERNAL(ht_OSC);
